c8313611f62c604166a719869115ebbf61ca04ac
Ageng Setyo Nugroho <setyo.nugroho96@gmail.com>
2021-08-14 20:19:18 +0700

    [Home] Create UI Home - Refactoring Routes - Delete About.vue - Add Picture flickr - Change UI Appbar - Add UI Search at Home

---
+++ src/components/Appbar/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar app>
+    <div class="d-flex align-center">
+      <v-img
+        alt="Flickr Logo"
+        class="shrink mr-2"
+        contain
+        :src="require('../../assets/images/flickr.png')"
+        transition="scale-transition"
+        width="60"
+      />
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,67 @@
 <template>
-  <hello-world />
+  <div>
+    <Appbar />
+    <div class="mx-4">
+      <div
+        class="
+          d-flex
+          flex-row
+          justify-space-between
+          header
+          my-6
+          pa-3
+          rounded-lg
+        "
+      >
+        <div style="width: 288px">
+          <v-text-field
+            v-model="search"
+            prepend-inner-icon="mdi-magnify"
+            placeholder="Search Tags"
+            hide-details
+            solo
+            dense
+            class="rounded-lg"
+          ></v-text-field>
+        </div>
+        <div style="width: 150px">
+          <v-select
+            v-model="sortBy"
+            :items="itemSortBy"
+            placeholder="Sort By"
+            solo
+            hide-details
+            dense
+            class="rounded-lg"
+            color="primary"
+            item-text="text"
+            item-value="value"
+          >
+            <template #item="{ item }">
+              <p class="selection-item ma-0">
+                <v-icon small class="mr-3">
+                  {{ item.icon }}
+                </v-icon>
+                <span>
+                  {{ item.text }}
+                </span>
+              </p>
+            </template>
+          </v-select>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "../components/HelloWorld";
+const Appbar = () => import("@/components/Appbar");
 
 export default {
   name: "Home",
 
   components: {
-    HelloWorld,
+    Appbar,
   },
 };
 </script>
