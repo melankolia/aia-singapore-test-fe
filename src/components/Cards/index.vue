3592ecfdca5817ede01242cdd761c654e98345cc
Ageng Setyo Nugroho <setyo.nugroho96@gmail.com>
2021-08-14 22:05:31 +0700

    [Cards] Integrate data detail to Component Cards

--- src/components/Cards/index.vue
+++ src/components/Cards/index.vue
@@ -1,34 +1,76 @@
 <template>
   <v-card class="my-4" max-width="340" style="width: 100%">
-    <v-img
-      height="250"
-      src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
-    ></v-img>
-    <v-card-title>Cafe Badilico</v-card-title>
-    <v-card-text>
-      <div>
-        Small plates, salads & sandwiches - an intimate setting with 12 indoor
-        seats plus patio seating.
-      </div>
+    <v-card-text v-if="postedByAvailable">
+      <div v-html="data.description[0]"></div>
+    </v-card-text>
+    <div class="container-image">
+      <v-img height="250" :src="data.media.m" />
+      <p class="date-text date pa-2 ma-2 rounded-lg primary white--text">
+        {{ data.published }}
+      </p>
+    </div>
+    <v-card-title>{{ data.title || "-" }}</v-card-title>
+    <v-card-text v-if="descriptionAvailable">
+      <div v-html="data.description[2]"></div>
     </v-card-text>
     <v-divider class="mx-4"></v-divider>
-    <v-card-text>
+    <v-card-text v-if="tagsAvailable">
       <div>Tag's</div>
       <v-chip-group active-class="primary white--text" column>
-        <v-chip small>5:30PM</v-chip>
-
-        <v-chip small>7:30PM</v-chip>
-
-        <v-chip small>8:00PM</v-chip>
-
-        <v-chip small>9:00PM</v-chip>
+        <v-chip small v-for="(tag, i) in data.tags" :key="i">{{
+          tag | toTitle
+        }}</v-chip>
       </v-chip-group>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
-export default {};
+const DataDetail = {
+  author: null,
+  author_id: null,
+  date_taken: null,
+  description: [],
+  link: null,
+  media: {
+    m: null,
+  },
+  published: null,
+  tags: [],
+  title: null,
+};
+
+export default {
+  props: {
+    data: {
+      type: Object,
+      required: true,
+      default: () => DataDetail,
+    },
+  },
+  computed: {
+    tagsAvailable() {
+      return this.data?.tags?.length > 0;
+    },
+    descriptionAvailable() {
+      return this.data?.description.length > 2 && this.data?.description[2];
+    },
+    postedByAvailable() {
+      return this.data?.description[0];
+    },
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.date {
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 1;
+}
+
+.container-image {
+  position: relative;
+}
+</style>
